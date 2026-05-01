7d8ea4cc0700cab3e06fb2d2b43ac08a837e9b1b
setsus-1216 <setsugin@gmail.com>
2026-05-01 19:36:19 +0700

    interaction (like, comment)

---
+++ InteractHub_API/Controllers/InteractionController.cs
@@ -0,0 +1,41 @@
+using InteractHub_API.DTOs.Interactions;
+using InteractHub_API.Services;
+using InteractHub_API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteractHub_API.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class InteractionController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    private readonly IInteractionService _interactionService;
+    public InteractionController(AppDbContext context, IInteractionService interactionService)
+    {
+        _context = context;
+        _interactionService = interactionService;
+    }
+
+    [HttpPost("AddComment")]
+    public async Task<IActionResult> AddComment([FromBody] CommentRequest request)
+    {
+       var response = await _interactionService.CommentAsync(request);
+       if (response.isSuccess)
+        {
+            return Ok(response);
+        }
+        return BadRequest(response);
+    }
+
+    [HttpPost("AddLike")]
+    public async Task<IActionResult> AddLike([FromBody] LikeRequest request)
+    {
+       var response = await _interactionService.LikeAsync(request);
+       if (response.isSuccess)
+        {
+            return Ok(response);
+        }
+        return BadRequest(response);
+    }
+}
