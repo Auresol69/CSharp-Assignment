3e09f60bef2c61c1872c0157827150c9f3471ffb
setsus-1216 <setsugin@gmail.com>
2026-05-01 19:34:56 +0700

    Interaction request (like, comment)

---
+++ InteractHub_API/DTOs/Interactions/LikeRequest.cs
@@ -0,0 +1,7 @@
+namespace  InteractHub_API.DTOs.Interactions;
+
+public class LikeRequest
+    {
+        public string IdTaiKhoan { get; set; } = string.Empty;
+        public string IdPost { get; set; } = string.Empty;
+    }
